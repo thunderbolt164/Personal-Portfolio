d2715088ddcd50bcec252d400ac1eee8061c070b
thunderbolt164 <dhruvagarwal@jklu.edu.in>
2026-08-03 16:42:16 +0530

    final

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -13,7 +13,7 @@ const handleProjectClick = (project) => {
     alert("🚧 This project is currently under development.");
   }
 };
-const ShowcaseSection = () => {
+const ShowCaseSection = () => {
   const sectionRef = useRef(null);
   const cardsRef = useRef([]);
 
@@ -92,4 +92,4 @@ const ShowcaseSection = () => {
   );
 };
 
-export default ShowcaseSection;
+export default ShowCaseSection;
