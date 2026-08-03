53842d079d2d2fa5820a7c1635e043c08d9047bf
thunderbolt164 <dhruvagarwal@jklu.edu.in>
2026-08-03 17:04:20 +0530

    polished

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import {socialImgs} from '../constants/index'
+import React from "react";
+import { socialImgs } from "../constants/index";
+import { HiArrowDownTray } from "react-icons/hi2";
 const Footer = () => {
   return (
     <footer className="footer">
-        <div className= "footer-container">
-            <div className='flex flex-col justify-center md:items-start items-center'>
-                
-            </div>  
-            <div className='socials'>
-                {socialImgs.map((img)=>(
-                    <a className="icon" target="_blank" href={img.url} key={img.url}>
-                        <img src={img.imgPath}/>   
-                    </a>
-                ))}
-            </div>
-
-            <div className='flex flex-col justify-center'>
-                <p className="text-center md:text-end">
-                        ©{new Date().getFullYear() }Dhruv Aakash. All rights reversed.
-                </p>
-            </div>
+      <div className="footer-container">
+        <div className="flex flex-col justify-center md:items-start items-center">
+          <a
+  href="/Resume.pdf"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="hover:text-white transition-colors"
+>
+  View Resume
+</a>
         </div>
-    </footer>
-  )
-}
+        <div className="socials">
+          {socialImgs.map((img) => (
+            <a className="icon" target="_blank" href={img.url} key={img.url}>
+              <img src={img.imgPath} />
+            </a>
+          ))}
+        </div>
 
-export default Footer
+        <div className="flex flex-col justify-center">
+          <p className="text-center md:text-end">
+            ©{new Date().getFullYear()}Dhruv Aakash. All rights reversed.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
